5c58bbc3ae8a5055f2bf3ab937b3b1793c386241
Shuli Jones <srothbergjones@gmail.com>
2020-07-15 18:31:57 +0000

    Fixed bug and cleaned up FindMeetingQuery

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -20,7 +20,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
@@ -28,14 +27,13 @@ public final class FindMeetingQuery {
     long longDuration = request.getDuration();
     ArrayList<TimeRange> possibleTimes = new ArrayList<TimeRange>();
 
-    // Deal with special cases:
-    // Meeting is too long, impossible to ever hold it
+    // Check if meeting can fit within a day (otherwise impossible to hold it)
     if (longDuration > TimeRange.WHOLE_DAY.duration()) {
       return possibleTimes;
     }
-    // No events/no attendees, hold meeting anytime
-    if (events.size() == 0 || attendees.size() == 0) {
-      possibleTimes.add(TimeRange.fromStartDuration(TimeRange.START_OF_DAY, TimeRange.WHOLE_DAY.duration()));
+    // No attendees, hold meeting anytime
+    if (attendees.isEmpty()) {
+      possibleTimes.add(TimeRange.WHOLE_DAY);
       return possibleTimes;
     }
  
@@ -44,96 +42,67 @@ public final class FindMeetingQuery {
 
     // Filter events so that it only includes events that have one or more
     // attendees who are coming to this event
-    ArrayList<Event> eventsByStart = new ArrayList<Event>();
-    for (Event event : events) {
-      HashSet<String> eventAttendees = new HashSet<String>(event.getAttendees());
-      HashSet<String> meetingAttendees = new HashSet<String>(attendees);
-      eventAttendees.retainAll(meetingAttendees); // set intersection
-      if (eventAttendees.size() > 0) {
-        eventsByStart.add(event);
-      }
-    }
+    List<Event> eventsByStart = new ArrayList<>(getRelevantEvents(attendees, events));
     Collections.sort(eventsByStart, Event.ORDER_BY_START_TIME);   
 
-    // Check again to see if there are any events
-    if (eventsByStart.size() == 0 ) {
-      possibleTimes.add(TimeRange.fromStartDuration(TimeRange.START_OF_DAY, TimeRange.WHOLE_DAY.duration()));
+    // If there are no events including our meeting attendees, can hold anytime
+    if (eventsByStart.isEmpty() ) {
+      possibleTimes.add(TimeRange.WHOLE_DAY);
       return possibleTimes;
     }
 
     // Check if we can have a meeting before the earliest-starting event
     int earliestStart = eventsByStart.get(0).getWhen().start();
-    if (TimeRange.START_OF_DAY < earliestStart && earliestStart - TimeRange.START_OF_DAY >= duration) {
+    if (TimeRange.START_OF_DAY < earliestStart && 
+      earliestStart - TimeRange.START_OF_DAY >= duration) {
       possibleTimes.add(TimeRange.fromStartEnd(
           TimeRange.START_OF_DAY, earliestStart, false));
     }
 
-    // Check in between events - we know we can't have anything until
-    // the earliest-starting event ends, so find the earliest-starting event
-    // after that end time and see if the meeting fits between them
-    Event currentEarliestEvent = eventsByStart.get(0);
-    Event nextEvent;
-    while (true) {
-      // Try to get event
-      Optional<Event> potentialNextEvent = getNextEarliestEvent(
-          currentEarliestEvent.getWhen().end(), eventsByStart, 0, eventsByStart.size()-1);
+    // Check in between events
+    Event currentEvent = eventsByStart.get(0);
+    int latestEnd = currentEvent.getWhen().end();
 
-      if (!potentialNextEvent.isPresent()) { 
-        break; // No event starts after current one ends
-      }
-
-      // Check if we can fit the meeting between the two events
-      // ***BUG***
-      nextEvent = potentialNextEvent.get();
+    for (int i = 1; i < eventsByStart.size(); i++) {
+      Event nextEvent = eventsByStart.get(i);
+      int nextEventEnd = nextEvent.getWhen().end();
       int nextEventStart = nextEvent.getWhen().start();
-      int currentEventEnd = currentEarliestEvent.getWhen().end();
+      int currentEventEnd = currentEvent.getWhen().end();
+
+      if (nextEventEnd < latestEnd) {
+        continue; //there is still another event ongoing
+      }
+      latestEnd = nextEventEnd;
+
       if (nextEventStart - currentEventEnd >= duration) {
         possibleTimes.add(TimeRange.fromStartEnd(
             currentEventEnd, nextEventStart, false));
       }
-      currentEarliestEvent = nextEvent;
+      currentEvent = nextEvent;
     }
 
 
-    // Lastly, check if we can have a meeting before the latest-ending event
-    ArrayList<Event> eventsByEnd = new ArrayList<Event>(eventsByStart);
-    Collections.sort(eventsByEnd, Event.ORDER_BY_END_TIME);
-    int latestEventEnd = eventsByEnd.get(eventsByEnd.size()-1).getWhen().end();
-    if (TimeRange.END_OF_DAY - latestEventEnd >= duration) {
+    // Lastly, see if we can have a meeting between the last event end and the end of the day
+    if (TimeRange.END_OF_DAY - latestEnd >= duration) {
       possibleTimes.add(TimeRange.fromStartEnd(
-        latestEventEnd, TimeRange.END_OF_DAY, true));
+        latestEnd, TimeRange.END_OF_DAY, true));
     }
 
     return possibleTimes;
   }
 
-  // Parameters: int endTime, representing a time in minutes 
-  //     List<Event>, a list of events which must be already sorted by start time
-  //     ints low and high, marking off the section of list of events to be searched
-  // Returns: the earliest-starting event that starts at or after endTime,
-  // or an empty optional if there is no such event listed
-  private Optional<Event> getNextEarliestEvent(int endTime, 
-      List<Event> events, int low, int high) {
-    
-    // Base cases
-    if (events.get(low).getWhen().start() >= endTime) {
-      // The first event is the earliest, so if it works, we're done
-      return Optional.of(events.get(low));
+  // Given a list of events and a list of attendees, return a filtered list
+  // of events such that all events have at least one attendee from attendees
+  private Collection<Event> getRelevantEvents(Collection<String> attendees, Collection<Event> events) {
+    ArrayList<Event> filteredEvents = new ArrayList<Event>();
+    for (Event event : events) {
+      HashSet<String> eventAttendees = new HashSet<String>(event.getAttendees());
+      HashSet<String> meetingAttendees = new HashSet<String>(attendees);
+      eventAttendees.retainAll(meetingAttendees); // set intersection
+      if (!eventAttendees.isEmpty()) {
+        filteredEvents.add(event);
+      }
     }
-    if (low == high || events.get(high).getWhen().start() < endTime) {
-      // If low == high, only one event (which we know doesn't work
-      // or we'd have returned already); if latest-starting event 
-      // doesn't work, we know none will
-      return Optional.empty();
-    }
-
-    // Neither of our base cases conditions are met, 
-    // so binary search the list recursively
-    int mid = (low + high) / 2;
-    if (events.get(mid).getWhen().start() < endTime) {
-      return getNextEarliestEvent(endTime, events, mid+1, high);
-    } else {
-      return getNextEarliestEvent(endTime, events, low, mid);
-    }
+    return filteredEvents;
   }
-}
+} 
