5a211f3425584e12c7edcfbdb059b9854b72bb5e
Shuli Jones <srothbergjones@gmail.com>
2020-07-20 14:18:16 +0000

    Minor formatting fixes

--- walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
+++ walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
@@ -188,13 +188,12 @@ public final class FindMeetingQueryTest {
             Arrays.asList(PERSON_A)),
         new Event("Event 2", TimeRange.fromStartDuration(TIME_0900AM, DURATION_30_MINUTES),
             Arrays.asList(PERSON_B)),
-        new Event("Event 3", TimeRange.fromStartDuration(TIME_0830AM,
-        DURATION_30_MINUTES), 
+        new Event("Event 3", TimeRange.fromStartDuration(TIME_0830AM,DURATION_30_MINUTES), 
             Arrays.asList(PERSON_C)));
 
     MeetingRequest request =
         new MeetingRequest(Arrays.asList(PERSON_A, PERSON_B), DURATION_30_MINUTES);
-      request.addOptionalAttendee(PERSON_C);
+    request.addOptionalAttendee(PERSON_C);
 
     Collection<TimeRange> actual = query.query(events, request);
     Collection<TimeRange> expected =
@@ -411,7 +410,8 @@ public final class FindMeetingQueryTest {
 
   @Test
   public void bothOptionalsTooBusy() {
-    // No mandatory attendees, and two optional attendees who are busy all day. // The result should be that there is no possible meeting time.
+    // No mandatory attendees, and two optional attendees who are busy all day. 
+    // The result should be that there is no possible meeting time.
 
     Collection<Event> events = Arrays.asList(
         new Event("Event 1", TimeRange.WHOLE_DAY,
